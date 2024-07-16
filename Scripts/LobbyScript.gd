08baa64affb7e68bb726e8c3dd960ba233b41089
soCalledJt <SaimirBrahja@outlook.com>
2024-07-17 00:10:08 +0200

    Created the Lobby

---
+++ Scripts/LobbyScript.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+
+func _on_play_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+	get_tree().quit() # Replace with function body.
