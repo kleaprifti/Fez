312d8b48bc155da0017ebae00cf922166b2367aa
soCalledJt <SaimirBrahja@outlook.com>
2024-08-13 14:53:05 +0200

    Added flipped player animations and key bindings

--- Scripts/playerScript.gd
+++ Scripts/playerScript.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,10 +17,23 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+		
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("Standing")
+		else:
+			animated_sprite.play("Walking")
+	else:
+		animated_sprite.play("Jumping")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
